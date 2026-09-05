3b5ea4ee5f96ee72565c6dbedbc18f8b751e85ab
Yuliya Zakrepa <yv.zakrepa@gmail.com>
2026-09-05 20:13:02 +0300

    add changes in note.js, authValidation.js

--- src/models/note.js
+++ src/models/note.js
@@ -11,6 +11,6 @@ const noteSchema = new Schema(
   { timestamps: true, versionKey: false },
 );
 
-noteSchema.index({ tag: 1 });
+noteSchema.index({ tag: 1, userId: 1 });
 
 export const Note = model('Note', noteSchema);
